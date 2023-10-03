ae23542bf57df37eda9e1c1608ab55aea5cc5d2e
Tony Collins <tjc@eltham-college.org.uk>
2023-10-03 11:47:15 +0100

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void TakeCourseworkIn(int mark) 
+        {
+            if (mark < 3) throw new RubbishCourseworkException("Sort it out you muppet!!!!!");
+            // Do something
+        }
+    }
+}
